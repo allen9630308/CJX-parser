936977f7ba1dccdb24d9bdba9ef3146afa7a31b2
allen9630308 <allen9630308@gmail.com>
2022-04-23 16:19:31 +0800

    220423 commit

---
+++ include/ll_string.hpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+bool visible(char c);
+
+bool upper(char c);
+
+int lower(char c);
+
+char toupper(char c);
+
+char tolower(char c);
+
+string one_strip(string s);
+
+string rvs(string s);
+
+string full_strip(string s);
